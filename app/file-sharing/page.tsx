6ab721ad0013a1d2e4ac61d303baa7a92e95fe18
Rupamjit Ghosh <rupamjitghosh51@gmail.com>
2025-09-07 15:08:24 +0530

    setup primsa

--- app/file-sharing/page.tsx
+++ app/file-sharing/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='flex items-center justify-center h-screen '>
-        <Input type='file'/>
+        <Input type='file' />
     </div>
   )
 }
